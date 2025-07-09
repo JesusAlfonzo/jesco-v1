a56ed0e8ac3c84382da87b74832d34dc827dfcbd
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2025-07-09 10:27:04 -0400

    Card Component Refactorization

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,24 +1,25 @@
-<div class="card {{ $class ?? '' }} w-auto mb-3 d-inline-flex m-3">
-  <div class="card-body ">
-    @if(isset($title) || $slot->has('title'))
-      <h5 class="card-title">
-        {{ $title ?? $slot->get('title') }}
-      </h5>
-    @endif
+<div class="card {{ $cardClass ?? '' }} mb-4" style="max-width: 100%; min-width: 250px;">
+    <div class="card-body">
+        @if (isset($title) || $slot->has('title'))
+            <h5 class="card-title">
+                {{ $title ?? $slot->get('title') }}
+            </h5>
+        @endif
 
-    <div class="card-text">
-      {{ $slot }}
+        <div class="card-text">
+            {{ $slot }}
+        </div>
+
+        @if (isset($button) || $slot->has('button'))
+            <div class="mt-4">
+                {{ $button ?? $slot->get('button') }}
+            </div>
+        @endif
     </div>
-
-    @if(isset($button) || $slot->has('button'))
-      <div class="mt-4">
-        {{ $button ?? $slot->get('button') }}
-      </div>
-    @endif
-  </div>
 </div>
 
 
+{{-- asi se aplica en el frontend --}}
 {{-- 
         <x-card
             title="Modulo: Marcas"
