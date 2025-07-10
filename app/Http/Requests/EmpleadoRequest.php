9b82a4443c6de9db70833c2323aa8a7484418448
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2025-07-10 09:09:47 -0400

    Se crearon mensajes de alerta en español para los request de creacion de empleados.

--- app/Http/Requests/EmpleadoRequest.php
+++ app/Http/Requests/EmpleadoRequest.php
@@ -30,4 +30,19 @@ class EmpleadoRequest extends FormRequest
 			'fecha_ingreso' => 'required',
         ];
     }
+    public function messages(): array
+{
+    return [
+        'nombre.required' => 'El nombre es obligatorio.',
+        'nombre.string' => 'El nombre debe ser una cadena de texto.',
+        'apellido.required' => 'El apellido es obligatorio.',
+        'apellido.string' => 'El apellido debe ser una cadena de texto.',
+        'departamento_id.required' => 'El departamento es obligatorio.',
+        'cargo.required' => 'El cargo es obligatorio.',
+        'cargo.string' => 'El cargo debe ser una cadena de texto.',
+        'estado.required' => 'El estado es obligatorio.',
+        'fecha_ingreso.required' => 'La fecha de ingreso es obligatoria.',
+    ];
+}
+
 }
